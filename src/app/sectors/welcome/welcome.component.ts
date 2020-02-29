36b5528b9989aec2d0f9580131de3ce31869bc95
Lukasz Winowicz <37307177+WinowiczL@users.noreply.github.com>
2020-02-29 16:39:03 +0100

    updated

--- src/app/sectors/welcome/welcome.component.ts
+++ src/app/sectors/welcome/welcome.component.ts
@@ -1,4 +1,5 @@
-import {Component} from '@angular/core';
+import {Component, Inject} from '@angular/core';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-welcome',
@@ -6,5 +7,21 @@ import {Component} from '@angular/core';
   styleUrls: ['./welcome.component.scss']
 })
 export class WelcomeComponent {
+
+  isNavChecked: boolean;
+
+  element: HTMLElement;
+  constructor(@Inject(DOCUMENT) document) {
+  }
+
+  changeNavChecked() {
+    this.isNavChecked = !this.isNavChecked;
+  }
+
+  goToView(view: string) {
+    this.isNavChecked = false;
+    this.element = document.getElementById(`${view}`) as HTMLElement;
+    this.element.scrollIntoView({ behavior: 'smooth' });
+  }
 
 }
