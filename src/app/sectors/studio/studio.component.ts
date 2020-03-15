a12fbdc663f9adfc9bef467cd5263fe788850cb3
Lukasz Winowicz <37307177+WinowiczL@users.noreply.github.com>
2020-03-15 17:53:23 +0100

    updated

--- src/app/sectors/studio/studio.component.ts
+++ src/app/sectors/studio/studio.component.ts
@@ -1,9 +1,17 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-studio',
   templateUrl: './studio.component.html',
   styleUrls: ['./studio.component.scss']
 })
-export class StudioComponent {
+export class StudioComponent implements OnInit {
+
+  mobile: boolean;
+
+  ngOnInit() {
+    if (window.screen.width < 768) {
+      this.mobile = true;
+    }
+  }
 }
