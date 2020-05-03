f8b1ddb20dc6358f66eb0155b176cf8d5a85e168
Lukasz Winowicz <37307177+WinowiczL@users.noreply.github.com>
2020-05-03 15:09:53 +0200

    u2

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/photography2'));
+app.use(express.static(__dirname + '/dist/elizamakiej'));
 
 app.get('/*', function(req,res) {
 
-  res.sendFile(path.join(__dirname+'/dist/photography2/index.html'));
+  res.sendFile(path.join(__dirname+'/dist/elizamakiej/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
